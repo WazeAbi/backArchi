ff6fc41df72632b7220923ec36568c6bf4b85f99
Cecile HURIAUX <cecile.huriaux@gmail.com>
2024-03-12 16:24:43 +0100

    feat(Entity) création de l'entité Produit

--- arski/src/main/java/co/simplon/arski/arski/persistance/entity/Produit.java
+++ arski/src/main/java/co/simplon/arski/arski/persistance/entity/Produit.java
@@ -1,5 +1,63 @@
 package co.simplon.arski.arski.persistance.entity;
 
-public class Produit {
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Table(name = "produits")
+public class Produit {
+    @Id
+    private int id;
+
+    @Column(name = "nom")
+    private String nom;
+
+    @Column(name = "prix")
+    private double prix;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "categorie")
+    private String categorie;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
 }

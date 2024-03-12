e60c625d2e09b352f384939df40dda2d8e33f325
WazeAbi <alexdalle12@gmail.com>
2024-03-12 14:00:28 +0100

    Build(SpringInitializr): Creation projet

---
+++ arski/src/main/java/co/simplon/arski/arski/ArskiApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.arski.arski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArskiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArskiApplication.class, args);
+    }
+
+}
---
+++ arski/src/test/java/co/simplon/arski/arski/ArskiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.simplon.arski.arski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArskiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

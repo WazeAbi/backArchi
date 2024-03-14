22dd1fd5c337acaa7e937601b6ae400e7c333260
Désiré <github.freebie042@passmail.net>
2024-03-14 14:08:53 +0100

    feat(Recuperer): création du contrôleur pour récupérer les produits par id

---
+++ arski/src/main/java/co/simplon/arski/arski/presentation/controller/RecupererProduitController.java
@@ -0,0 +1,24 @@
+package co.simplon.arski.arski.presentation.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.simplon.arski.arski.business.dto.ProduitDTO;
+import co.simplon.arski.arski.business.service.IProduitService;
+
+@RestController
+public class RecupererProduitController {
+    private IProduitService produitService;
+
+    @GetMapping("/produits/{id}")
+    public ProduitDTO recupererParId(@PathVariable Integer id) {
+        return produitService.recuperParId(id);
+    }
+
+    @Autowired
+    public void setProduitService(IProduitService produitService) {
+        this.produitService = produitService;
+    }
+}
